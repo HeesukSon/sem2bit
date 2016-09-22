6ebaa175a9fcb04f583525d69492a8de2cb03743
Heesuk Son <heesuk.chad.son@gmail.com>
2016-09-22 17:45:04 +0900

    third commit

---
+++ src/heesuk/percom/sherlock/io/kb/probtree/ProbTreeEdge.java
@@ -0,0 +1,40 @@
+package heesuk.percom.sherlock.io.kb.probtree;
+
+public class ProbTreeEdge {
+	private float weight;
+	private ProbTreeNode prev;
+	private ProbTreeNode next;
+	
+	public ProbTreeEdge(){
+		
+	}
+	
+	public ProbTreeEdge(float weight){
+		this();
+		this.weight = weight;
+	}
+	
+	public void setWeight(float weight){
+		this.weight = weight;
+	}
+	
+	public float getWeight(){
+		return this.weight;
+	}
+
+	public ProbTreeNode getPrev() {
+		return prev;
+	}
+
+	public void setPrev(ProbTreeNode prev) {
+		this.prev = prev;
+	}
+
+	public ProbTreeNode getNext() {
+		return next;
+	}
+
+	public void setNext(ProbTreeNode next) {
+		this.next = next;
+	}
+}
