cfc0db6db27bee0250f815bacb364c0c0e858d5a
Heesuk Son <heesuk.chad.son@gmail.com>
2016-09-23 17:36:55 +0900

    slpv2 added

--- src/heesuk/percom/sherlock/io/kb/sdp/SDPKBUtil.java
+++ src/heesuk/percom/sherlock/io/kb/sdp/SDPKBUtil.java
@@ -32,20 +32,30 @@ public class SDPKBUtil {
 		SDP slp1 = new SDP("SLPv1");
 		SDPMessage msg = new SDPMessage();
 		msg.addField(new MessageField("Version", MessageFieldType.VERSION_INFO, MessageFieldLocation.HEADER, 8));
-		msg.addField(new MessageField("Function", MessageFieldType.VERSION_INFO, MessageFieldLocation.HEADER, 8));
-		msg.addField(new MessageField("Length", MessageFieldType.VERSION_INFO, MessageFieldLocation.HEADER, 8));
-		msg.addField(new MessageField("Control", MessageFieldType.VERSION_INFO, MessageFieldLocation.HEADER, 8));
-		msg.addField(new MessageField("Language Code", MessageFieldType.VERSION_INFO, MessageFieldLocation.HEADER, 8));
-		msg.addField(new MessageField("Char Encoding", MessageFieldType.VERSION_INFO, MessageFieldLocation.HEADER, 8));
-		msg.addField(new MessageField("XID", MessageFieldType.VERSION_INFO, MessageFieldLocation.HEADER, 8));
-		msg.addField(new MessageField("Language Tag Length", MessageFieldType.VERSION_INFO, MessageFieldLocation.HEADER, 8));
-		msg.addField(new MessageField("Language Tag", MessageFieldType.VERSION_INFO, MessageFieldLocation.HEADER, 8));
+		msg.addField(new MessageField("Function", MessageFieldType.MESSAGE_TYPE, MessageFieldLocation.HEADER, 8));
+		msg.addField(new MessageField("Length", MessageFieldType.MESSAGE_LENGTH, MessageFieldLocation.HEADER, 16));
+		msg.addField(new MessageField("Control", MessageFieldType.CONTROL_FLAG, MessageFieldLocation.HEADER, 16));
+		msg.addField(new MessageField("Language Code", MessageFieldType.LANGUAGE_CODE, MessageFieldLocation.HEADER, 16));
+		msg.addField(new MessageField("Char Encoding", MessageFieldType.ENCODING, MessageFieldLocation.HEADER, 8));
+		msg.addField(new MessageField("XID", MessageFieldType.SESSION_MGMT, MessageFieldLocation.HEADER, 16));
 		
 		slp1.addMessage(msg);
+		this.sdpMap.put(slp1.getName(), slp1);
 	}
 	
 	public void addSLPv2() {
-
+		SDP slp2 = new SDP("SLPv2");
+		SDPMessage msg = new SDPMessage();
+		msg.addField(new MessageField("Version", MessageFieldType.VERSION_INFO, MessageFieldLocation.HEADER, 8));
+		msg.addField(new MessageField("Function", MessageFieldType.MESSAGE_TYPE, MessageFieldLocation.HEADER, 8));
+		msg.addField(new MessageField("Length", MessageFieldType.MESSAGE_LENGTH, MessageFieldLocation.HEADER, 16));
+		msg.addField(new MessageField("Control", MessageFieldType.CONTROL_FLAG, MessageFieldLocation.HEADER, 16));
+		msg.addField(new MessageField("Language Code", MessageFieldType.LANGUAGE_CODE, MessageFieldLocation.HEADER, 16));
+		msg.addField(new MessageField("Char Encoding", MessageFieldType.ENCODING, MessageFieldLocation.HEADER, 8));
+		msg.addField(new MessageField("XID", MessageFieldType.SESSION_MGMT, MessageFieldLocation.HEADER, 16));
+		
+		slp2.addMessage(msg);
+		this.sdpMap.put(slp2.getName(), slp2);
 	}
 
 	public void addUPnPv1() {
